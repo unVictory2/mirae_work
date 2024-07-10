fff27a9f09914c562acf425209066c31029f854e
unvictory2 <seungeon7878@gmail.com>
2024-07-11 01:11:07 +0900

    deleting submodule

--- alstarboard/src/main/java/com/example/alstarboard/repository/BoardMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.alstarboard.repository;
-
-import com.example.alstarboard.domain.Board;
-
-import java.util.List;
-
-public interface BoardMapper {
-    public List<Board> getBoardList();
-}
